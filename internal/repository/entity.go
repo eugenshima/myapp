4d258dd88985c23985527efde27d8529e63d9176
eugenshima <eugenshima@mail.ru>
2023-06-21 11:20:51 +0300

    trying repo

---
+++ internal/repository/entity.go
@@ -0,0 +1,5 @@
+package repository
+
+func Greet() string {
+	return "Greetings traveller"
+}
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	//"myapp/internal/repository"
 	"net/http"
 
+	"github.com/eugenshima/myapp/internal/repository"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	str := repository.Greet()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, str)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
